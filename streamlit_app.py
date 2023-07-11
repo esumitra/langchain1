d50868eaa3086a0816236a7a2e01ca5544c8a369
esumitra <edward.sumitra@gmail.com>
2023-07-11 19:18:26 -0400

    initial checkin

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from langchain.llms import OpenAI
+
+st.title('Ed\'s Quickstart App')
+
+open_ai_key = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=open_ai_key, model="text-davinci-003", client="a")
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter your question here')
+  submitted = st.form_submit_button('Submit')
+  generate_response(text)
+

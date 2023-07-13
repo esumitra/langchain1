a8ddcc3a64f1ad4e88ce429cf8af594cf67fd973
esumitra <edward.sumitra@gmail.com>
2023-07-12 23:45:27 -0400

    add instructions to run in README

---
+++ README.md
@@ -0,0 +1,10 @@
+## AI Algebra Study Buddy
+An AI Algebra Study Buddy that can help school students with their math practice. The buddy is built with Chat-GPT and LangChain.
+
+
+### Installation
+
+### License
+Copyright 2023, esumitra
+
+Licensed under the MIT License.
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,19 @@ from langchain.llms import OpenAI
 
 st.title('Ed\'s Quickstart App')
 
-open_ai_key = st.sidebar.text_input('OpenAI API Key')
+open_ai_key:str = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text):
+def generate_response(input_text:str) -> None:
   llm = OpenAI(temperature=0.7, openai_api_key=open_ai_key, model="text-davinci-003", client="a")
   st.info(llm(input_text))
 
 with st.form('my_form'):
   text = st.text_area('Enter your question here', value='Explain how to solve a quadratic equation to a middle school student with steps')
   submitted = st.form_submit_button('Submit')
-  generate_response(text)
+  if not open_ai_key.startswith('sk-'):
+    st.warning('Please enter your Open API key', icon='⚠')
+  if submitted and open_ai_key.startswith('sk-'):
+    generate_response(text)
+
+
 

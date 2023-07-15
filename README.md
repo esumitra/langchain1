1583ec8a62fcb8db8ea57de6b9909c8d3d67de7d
esumitra <edward.sumitra@gmail.com>
2023-07-15 07:53:56 -0400

    add prompt template

--- README.md
+++ README.md
@@ -2,7 +2,31 @@
 An AI Algebra Study Buddy that can help school students with their math practice. The buddy is built with Chat-GPT and LangChain.
 
 
-### Installation
+### Install developer environment
+1. Create a local environment for the project
+e.g.,
+```
+conda create -n chatgpt1 python=3.9
+conda use chatgpt1
+```
+
+2. Install the dependencies
+
+```
+echo -e "streamlit\nopenai\nlangchain" > requirements.txt
+pip install -r requirements.txt
+```
+
+3. Run the app locally
+
+`streamlit run streamlit_app.py`
+
+### Deploy to Streamlit Cloud
+To deploy to streamlit cloud
+1. Create a public repo on github
+2. Connect streamlit could to the github account in #1
+3. Create a new streamlit app from the github repo and main branch
+4. Push changes to the main branch
 
 ### License
 Copyright 2023, esumitra
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,34 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from streamlit.logger import get_logger
 
-st.title('Ed\'s Quickstart App')
+log = get_logger(__name__)
+
+log.info('starting app')
+
+st.title('AI-Den, Your Algebra Study Buddy')
 
 open_ai_key:str = st.sidebar.text_input('OpenAI API Key')
 
-def generate_response(input_text:str) -> None:
+def generate_response(topic:str) -> str:
   llm = OpenAI(temperature=0.7, openai_api_key=open_ai_key, model="text-davinci-003", client="a")
-  st.info(llm(input_text))
+  template = 'As an experienced middle school teacher explain with detailed steps the algebra topic {topic}.'
+  prompt = PromptTemplate(input_variables=['topic'], template=template)
+  prompt_query = prompt.format(topic = topic)
+  log.info('calling OpenAPI with text %s', prompt_query)
+  response: str = llm(prompt_query)
+  return response
+  
 
 with st.form('my_form'):
-  text = st.text_area('Enter your question here', value='Explain how to solve a quadratic equation to a middle school student with steps')
+  text = st.text_area('What topic would you like to learn about?', value='solving equations')
   submitted = st.form_submit_button('Submit')
   if not open_ai_key.startswith('sk-'):
     st.warning('Please enter your Open API key', icon='⚠')
   if submitted and open_ai_key.startswith('sk-'):
-    generate_response(text)
+    res:str = generate_response(text)
+    st.info(res)
 
 
 

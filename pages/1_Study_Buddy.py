aeda0b98f394781634a333e72503b1b2a599b0c4
esumitra <edward.sumitra@gmail.com>
2024-03-01 22:09:15 -0500

    fix for Study Buddy demo

--- pages/1_Study_Buddy.py
+++ pages/1_Study_Buddy.py
@@ -1,8 +1,16 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.utils.utils import convert_to_secret_str
+
 from streamlit.logger import get_logger
 import configparser 
+import os
 
 # global objects
 # log: streamlit logger
@@ -34,7 +42,7 @@ with st.sidebar:
 
 def get_template_for_person(person: str) -> str:
   person_key:str = config.get('personality_names', person)
-  template: str = config.get('personality_templates', person_key)
+  template:str = config.get('personality_templates', person_key)
   log.debug('using personality template: %s', template)
   return template
 
@@ -42,13 +50,26 @@ def generate_response(topic:str, person:str) -> str:
   model_name:str = config.get('openai', 'model_name')
   client_id:str = config.get('openai', 'client_name')
   log.info('using model %s', model_name)
-  llm = OpenAI(temperature=0.7, openai_api_key=open_ai_key, model=model_name, client=client_id)
-  template = get_template_for_person(person)
-  prompt = PromptTemplate(input_variables=['topic'], template=template)
-  prompt_query = prompt.format(topic = topic)
-  log.info('calling OpenAPI with text %s', prompt_query)
-  response: str = llm(prompt_query)
-  return response
+  llm = ChatOpenAI(
+    name=client_id,
+    temperature=0,
+    api_key=convert_to_secret_str(open_ai_key),
+    model=model_name
+  )
+
+  system_message_prompt_template = 'You are a helpful assistant that helps students with their homework.'
+  system_message_prompt = SystemMessagePromptTemplate.from_template(system_message_prompt_template)
+  human_message_prompt = HumanMessagePromptTemplate.from_template(get_template_for_person(person))
+  chat_prompt = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+  )
+  formatted_messages = chat_prompt \
+    .format_prompt(topic=topic) \
+    .to_messages()
+  log.info('using messages\n%s', formatted_messages)
+  # get a chat completion from the formatted messages
+  result = llm(formatted_messages)
+  return result.content
 
 with st.form('my_form'):
   text = st.text_area('What topic would you like to learn about?', value='solve 5x + 3y = 3')
